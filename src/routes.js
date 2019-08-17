d8583662b11a9c42a57c6753ee9e8c7aa92b96f7
Alan Nascimento <alan.silva.n@hotmail.com>
2019-08-17 17:21:14 -0300

    404 error

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const routes = Router();
 
 routes.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 routes.get('/invoices', (req, res) => res.json(invoices));
+routes.get('*', (req, res) => res.send('404 Page not found'));
 
 export default routes;
