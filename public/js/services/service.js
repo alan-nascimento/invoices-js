e9f58bde6ca04b864fc2fcb8a651c9e0a0230e93
Alan Nascimento <alan.silva.n@hotmail.com>
2019-08-18 18:26:16 -0300

    Functions compose

--- public/js/services/service.js
+++ public/js/services/service.js
@@ -1,23 +1,23 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize, pipe } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/invoices';
 
-const sumItems = (code) => (invoices) => invoices
-  .flatMap((invoice) => invoice.items)
-  .filter((invoice) => invoice.code === code)
-  .reduce((total, item) => total + item.value, 0);
+const getItemsFromInvoices = (invoices) => invoices.flatMap((invoice) => invoice.items);
+const filterItemsByCode = (code, items) => items.filter((item) => item.code === code);
+const sumItemsValue = (items) => items.reduce((total, item) => total + item.value, 0);
 
 const invoicesService = {
   listAll() {
     return fetch(API)
       .then(handleStatus)
-      .catch((err) => {
-        console.log(err);
-        return Promise.reject(new Error('Unable to get invoices.'));
-      });
+      .catch((err) => Promise.reject(new Error(`Unable to get invoices. ${err}`)));
   },
   sumItems(code) {
-    return this.listAll().then(sumItems(code));
+    const filterItems = partialize(filterItemsByCode, code);
+    const sumItems = pipe(getItemsFromInvoices, filterItems, sumItemsValue);
+
+    return this.listAll().then(sumItems);
   },
 };
 
